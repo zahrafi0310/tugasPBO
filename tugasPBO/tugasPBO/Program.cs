2f92969044f761918a0ec44c40d6a6fc9a8c239e
Zahra Fidela <zahrafi0310@gmail.com>
2025-03-03 09:13:41 +0700

    Add files via upload

---
+++ tugasPBO/tugasPBO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string FirstName = "Zahra";
+string SecName = "Fidela";
+string ThirdName = "Ramadhiani";
+
+Console.WriteLine($"My full name is {FirstName} {SecName} {ThirdName}");
